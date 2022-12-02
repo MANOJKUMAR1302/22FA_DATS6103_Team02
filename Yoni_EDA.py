14077c026bf1823be002e1fbbb2f421392bdf7e9
Nayaeun <110697102+Nayaeun@users.noreply.github.com>
2022-12-02 10:25:56 -0500

    Update Yoni_EDA.py

--- Yoni_EDA.py
+++ Yoni_EDA.py
@@ -6,30 +6,36 @@ import matplotlib.pyplot as plt
 
 # %% 
 # Check dataset
-df = pd.read_csv("salary_data.csv")
+df = pd.read_csv("salary_data")
 df.info()
 df.isnull().sum()
 df.head()
 
 
 list(set(list(df['title'])))
-len(list(set(list(df['title'])))
-)
-list(set(list(df['location'])))
-len(list(set(list(df['location']))))
+# ['Management Consultant', 'Marketing', 'Product Designer', 'Human Resources', 'Product Manager', 'Software Engineering Manager', 'Solution Architect', 'Business Analyst', 'Sales', 'Hardware Engineer', 'Mechanical Engineer', 'Recruiter', 'Data Scientist' 'Technical Program Manager', 'Software Engineer']
+len(list(set(list(df['title'])))) # 15
 
+list(set(list(df['location']))) 
+len(list(set(list(df['location'])))) # 1050
+list(df['location'])[0].split(', ')[1]
 
 # Preprocessing data
+
+
 # %%
 # Is salary affected by the role offered? 
 
 avgbytitle = df.groupby('title').mean()
-avgbytitle
+print (avgbytitle)
 
 avgsalary = avgsalary = df.groupby('title').mean()['yearlysalary'].reset_index()
+print (avgsalary)
 
+avgsalary.plot(kind = 'bar', x = 'title', y = 'yearlysalary', title = 'Average Salary per Job Title', figsize=(15,8))
+plt.show()
 
-
+# check correlation between yearofexperience and title 
 
 
 #%% 
